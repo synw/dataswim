93808c95abe65b8340e462e3f49c530a6be0e8fa
Win <synwe@yahoo.com>
2017-11-17 15:15:23 +0000

    Version 0.3.4

--- dataswim/__init__.py
+++ dataswim/__init__.py
@@ -6,7 +6,7 @@ from .report import Report
 from .errors import Errors
 from .messages import Messages
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 
 class DataSwim(Db, Df, Plot, Report, Errors, Messages):
