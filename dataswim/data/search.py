b3a85fc12b46102472a2dd2b97215bb0885df08f
Win <synwe@yahoo.com>
2018-02-13 12:29:02 +0000

    Add inplace operations for contains and exact methods

--- dataswim/data/search.py
+++ dataswim/data/search.py
@@ -12,12 +12,28 @@ class Search():
         """
         self.df = df
 
-    def contains_(self, column, value):
+    def contains(self, column, value):
         """
-        Returns rows that contains a string value in a column
+        Returns a dataframe instance with rows that contains a string
+        value in a column
         """
         try:
-            df = self.df[self.df[column].str.contains(value) == True]
+            df = self._contains(column, value)
+            return df
+        except KeyError:
+            self.err(self.contains, "Can not find " +
+                     colors.bold(column) + " column")
+            return
+        except Exception as e:
+            self.err(e, self.contains, "Can not select contained data")
+
+    def contains_(self, column, value):
+        """
+        Returns a Dataswim instance with rows that contains a string
+        value in a column
+        """
+        try:
+            df = self._contains(column, value)
             return self.clone_(df.copy())
         except KeyError:
             self.err(self.contains_, "Can not find " +
@@ -26,13 +42,42 @@ class Search():
         except Exception as e:
             self.err(e, self.contains_, "Can not select contained data")
 
-    def exact_(self, column, *values):
+    def _contains(self, column, value):
         """
-        Returns rows that has the exact string value in a column
+        Returns a dataframe with rows that contains a string value in a column
         """
         try:
-            df2 = self.df[column].isin(values)
-            df = self.df[df2]
+            df = self.df[self.df[column].str.contains(value) == True]
+            return df
+        except KeyError:
+            self.err(self._contains, "Can not find " +
+                     colors.bold(column) + " column")
+            return
+        except Exception as e:
+            self.err(e, self._contains, "Can not select contained data")
+
+    def exact(self, column, *values):
+        """
+        Returns a dataframe with rows that has the exact string
+        value in a column
+        """
+        try:
+            df = self._exact(column, *values)
+            return df
+        except KeyError:
+            self.err(self.exact, "Can not find " +
+                     colors.bold(column) + " column")
+            return
+        except Exception as e:
+            self.err(e, self.exact, "Can not select exact data")
+
+    def exact_(self, column, *values):
+        """
+        Returns a Dataswim instance with rows that has the exact string
+        value in a column
+        """
+        try:
+            df = self._exact(column, *values)
             return self.clone_(df)
         except KeyError:
             self.err(self.exact_, "Can not find " +
@@ -40,3 +85,18 @@ class Search():
             return
         except Exception as e:
             self.err(e, self.exact_, "Can not select exact data")
+
+    def _exact(self, column, *values):
+        """
+        Returns rows that has the exact string value in a column
+        """
+        try:
+            df2 = self.df[column].isin(values)
+            df = self.df[df2]
+            return df
+        except KeyError:
+            self.err(self._exact_, "Can not find " +
+                     colors.bold(column) + " column")
+            return
+        except Exception as e:
+            self.err(e, self._exact_, "Can not select exact data")
